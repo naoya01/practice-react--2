4e6a478d12de880a6b18ad322412c086e8155d10
naoya <naoya.take10.01@gmail.com>
2022-04-20 20:40:23 +0900

    chilrArea updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,18 +3,21 @@ import { ChildArea } from "./ChildArea";
 import "./style.css";
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-  const onClickCountUp = () =>{
-    setCount(count + 1);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const onChangeText = (e) => setText(e.target.value)
+
+  const onClickOpen = () =>{
+    setOpen(!open)
   };
 
   return (
     <div className="App">
-      <input/>
+      <input value={text} onChange={onChangeText} />
       <br/>
       <br/>
-      <button>表示</button>
-      <ChildArea/>
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 };
