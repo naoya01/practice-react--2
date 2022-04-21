d54b0ae1814d40cab5472c5930a61336ce9f2e46
naoya <naoya.take10.01@gmail.com>
2022-04-21 21:39:05 +0900

    再レンダリング

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 import "./style.css";
 
@@ -7,9 +7,9 @@ export const App = () => {
   const [open, setOpen] = useState(false);
   const onChangeText = (e) => setText(e.target.value)
 
-  const onClickOpen = () =>{
-    setOpen(!open)
-  };
+  const onClickOpen = () =>{setOpen(!open)};
+
+  const onClickClose = useCallback(() => setOpen(false), [] );
 
   return (
     <div className="App">
@@ -17,7 +17,7 @@ export const App = () => {
       <br/>
       <br/>
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 };
