48c927b8fd54194812c6c934e859a69b2529cf3d
naoya <naoya.take10.01@gmail.com>
2022-04-25 16:40:50 +0900

    404ページ

--- react-router-practise/src/router/Router.jsx
+++ react-router-practise/src/router/Router.jsx
@@ -6,6 +6,7 @@ import { Page2 } from "../Page2";
 import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetailB";
 import { UrlParameter } from "../UrlParameter";
+import { Page404 } from "../Page404";
 export const Router = () => {
   return(
     <Routes>
@@ -17,6 +18,7 @@ export const Router = () => {
 
       <Route path="/page2" element={<Page2 />} />
       <Route path="/page2/:id" element={<UrlParameter />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 };
