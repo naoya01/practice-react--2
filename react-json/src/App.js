0f0c24178fced9c7504e5c2e5860187b027367b4
naoya <naoya.take10.01@gmail.com>
2022-05-02 14:22:38 +0900

    axios add

--- react-json/src/App.js
+++ react-json/src/App.js
@@ -6,11 +6,15 @@ import './App.css';
 function App() {
   const onClickUsers = () => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
-      console.log(res);
-    });
+      console.log(res.data);
+    })
+    .catch((err) => console.log(err));
   }
   const onClickUsers1 = () => {
-    alert("user1");
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1").then((res)=>{
+      console.log(res.data);
+    })
+    .catch((err) => console.log(err));
   }
   return (
     <div className="App">
