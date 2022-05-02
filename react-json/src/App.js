adffe28cd0b0976e92c94124e9f94970d5fb7165
naoya <naoya.take10.01@gmail.com>
2022-05-02 14:16:43 +0900

    axios add

--- react-json/src/App.js
+++ react-json/src/App.js
@@ -1,23 +1,21 @@
+
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
+      console.log(res);
+    });
+  }
+  const onClickUsers1 = () => {
+    alert("user1");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={onClickUsers}>users</button>
+        <button onClick={onClickUsers1}>id=1のuser</button> 
     </div>
   );
 }
