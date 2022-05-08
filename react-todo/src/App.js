7ed1f61a3dc4383a01d340cc39cb3b04f5f51e6e
naoya <naoya.take10.01@gmail.com>
2022-05-08 10:40:23 +0900

    onClickAdd

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -8,12 +8,14 @@ function App() {
   const [incompleteTodos, setIncompleteTodos] = useState(['aaa','iii']);
   const [completeTodos, setcompleteTodos] = useState(['uuu']);
   const onChangeTodoText = (event) => setTodoText(event.target.value)
+
+  const onClickAdd = ()  => alert(todoText);
   return (
     <>
       <div className="App">
         <div className="input-area">
           <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}></input>
-          <button onClick={InputTodo}>追加</button>
+          <button onClick={onClickAdd}>追加</button>
         </div>
 
         <div className="incomplete-area">
