473938c51aca65e5cf672087a212b5772f818cba
naoya <naoya.take10.01@gmail.com>
2022-05-10 17:37:14 +0900

    onClickBack

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -21,6 +21,23 @@ function App() {
     newTodos.splice(index,1);
     setIncompleteTodos(newTodos);
   }
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index,1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setcompleteTodos(newCompleteTodos);
+  };
+  
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index,1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setcompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  }
 
   return (
     <>
@@ -37,7 +54,7 @@ function App() {
               return (
             <div key={todo} className="list-row">
               <li>{todo}</li>
-              <button onClick={InputTodo}>完了</button>
+              <button onClick={ () => onClickComplete(index)}>完了</button>
               <button onClick={ () => onClickDelete(index)}>削除</button>
             </div>
               );
@@ -48,12 +65,11 @@ function App() {
         <div className="complete-area">
           <p className="title">完了のTODO</p>
           <ul>
-            {completeTodos.map((todo) => {
+            {completeTodos.map((todo,index) => {
               return(
             <div key={todo} className="list-row">
               <li>{todo}</li>
-              <button onClick={InputTodo}>完了</button>
-              <button onClick={InputTodo}>削除</button>
+              <button onClick={ () => onClickBack(index) }>戻す</button>
             </div>
               );
             })}
