0d66203756c2dcdf746b15f3613459d54a86539b
naoya <naoya.take10.01@gmail.com>
2022-05-11 13:19:31 +0900

    完成

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,83 +1,63 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { InputTodo } from "./components/InputTodo";
+import { CompleteTodo } from "./components/CompleteTodo";
+import { IncompleteTodo } from "./components/IncompleteTodo";
+
 
 function App() {
-  const InputTodo = () => {};
   const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([]);
-  const [completeTodos, setcompleteTodos] = useState(['uuu']);
-  const onChangeTodoText = (event) => setTodoText(event.target.value)
+  const [completeTodos, setcompleteTodos] = useState([]);
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
 
-  const onClickAdd = ()  => {
-    if (todoText === "" ) return;
+  const onClickAdd = () => {
+    if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
-    setTodoText("")
+    setTodoText("");
   };
 
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
-    newTodos.splice(index,1);
+    newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
-  }
+  };
   const onClickComplete = (index) => {
     const newIncompleteTodos = [...incompleteTodos];
-    newIncompleteTodos.splice(index,1);
+    console.log(newIncompleteTodos);
+    newIncompleteTodos.splice(index, 1);
 
     const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setIncompleteTodos(newIncompleteTodos);
     setcompleteTodos(newCompleteTodos);
   };
-  
+
   const onClickBack = (index) => {
     const newCompleteTodos = [...completeTodos];
-    newCompleteTodos.splice(index,1);
+    newCompleteTodos.splice(index, 1);
 
     const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
     setcompleteTodos(newCompleteTodos);
     setIncompleteTodos(newIncompleteTodos);
-  }
+  };
 
   return (
     <>
-      <div className="App">
-        <div className="input-area">
-          <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}></input>
-          <button onClick={onClickAdd}>追加</button>
-        </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
+      />
 
-        <div className="incomplete-area">
-          <p className="title">未完了のTODO</p>
-          <ul>
-            {incompleteTodos.map((todo,index) => {
-              return (
-            <div key={todo} className="list-row">
-              <li>{todo}</li>
-              <button onClick={ () => onClickComplete(index)}>完了</button>
-              <button onClick={ () => onClickDelete(index)}>削除</button>
-            </div>
-              );
-            })}
-          </ul>
-        </div>
+      <IncompleteTodo todos={incompleteTodos}  onClickDelete={onClickDelete} onClickComplete = {onClickComplete} />
 
-        <div className="complete-area">
-          <p className="title">完了のTODO</p>
-          <ul>
-            {completeTodos.map((todo,index) => {
-              return(
-            <div key={todo} className="list-row">
-              <li>{todo}</li>
-              <button onClick={ () => onClickBack(index) }>戻す</button>
-            </div>
-              );
-            })}
-
-          </ul>
-        </div>
-      </div>
+    
+      <CompleteTodo todos={completeTodos} onClickBack={onClickBack} />
     </>
+
   );
 }
 
