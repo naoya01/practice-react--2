befa271e1f365915021de48ffd6a751c89174d18
naoya <naoya.take10.01@gmail.com>
2022-05-13 12:41:20 +0900

    countDown

--- react-todo/src/components/CountUp.jsx
+++ react-todo/src/components/CountUp.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 export const CountUp = () => {
   const [count, setcount ] = useState(0);
-  const onClickCount = () => setcount(count + 1);
+  const onClickCountUp = () => setcount(count + 1);
+  const onClickCountDown = () => setcount(count - 1);
   return(
     <>
       <div>
         <p>{count}</p>
-        <button onClick={onClickCount}>up</button>
-        <button>down</button>
+        <button onClick={onClickCountUp}>up</button>
+        <button onClick={onClickCountDown}>down</button>
       </div>
     </>
   )
