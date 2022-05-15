40ae02a7e5f636f942ef4bfce4443741d1b41a4a
naoya <naoya.take10.01@gmail.com>
2022-05-15 19:45:11 +0900

    countDown

--- react-todo/src/components/CountUp.jsx
+++ react-todo/src/components/CountUp.jsx
@@ -4,12 +4,14 @@ export const CountUp = () => {
   const [count, setcount ] = useState(0);
   const onClickCountUp = () => setcount(count + 1);
   const onClickCountDown = () => setcount(count - 1);
+  const onClickCountReset = () => setcount(0);
   return(
     <>
       <div>
         <p>{count}</p>
         <button onClick={onClickCountUp}>up</button>
         <button onClick={onClickCountDown}>down</button>
+        <button onClick={onClickCountReset}>rest</button>
       </div>
     </>
   )
